6eb41244abf8f87fae9efd9e9a91b9d0bdc4a46b
soundargit <soundargit1@gmail.com>
2021-09-02 11:56:26 +0530

    Removed methods

--- app/src/main/java/com/test/mvvm/TestClass.java
+++ app/src/main/java/com/test/mvvm/TestClass.java
@@ -2,17 +2,5 @@ package com.test.mvvm;
 
 public class TestClass {
 
-    public void display(){
-        System.out.println("test");
-    }
-    public void add(){
-        System.out.println("test");
-    }
 
-    public void sub(){
-        System.out.println("sub");
-    }
-    public void mul(){
-        System.out.println("sub");
-    }
 }
